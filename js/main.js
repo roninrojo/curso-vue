d3029aac8e543acdb2ed86322f2513d2b3cef52a
acarazo <alberto.carazo@gmail.com>
2023-10-24 15:48:34 +0200

    Renderizado condicional Propiedades computadas

--- js/main.js
+++ js/main.js
@@ -4,20 +4,38 @@ const app = Vue.createApp({
     data() {
         return {
             search: null,
-            result: null
+            result: null,
+            error: null,
+            message: null,
+            favorites: new Map()
+        }
+    },
+    computed:{
+        isFavorite() {
+            return this.favorites.has(this.result.id)
         }
     },
     methods: {
         async doSearch() {
+            this.result = this.error = null;
             try {
                 const resp = await fetch(API + this.search);
-                const data = await resp.json()
+                if(!resp.ok) throw new Error("Sin resultado")
+                const data = await resp.json();
                 console.log(data);
-                this.result = true;
+                
+                this.result = data;
             } catch (error) {
-                console.log(error);
+                this.error = error;
                 this.result = false;
+                console.log(error);
             }
+        },
+        addFavorite() {
+            this.favorites.set(this.result.id, this.result)
+        },
+        removeFavorite() {
+            this.favorites.delete(this.result.id)
         }
     }
 }).mount('#app')
