e3de56f6e2ec6548f115628f9c4f2417e89ea031
acarazo <alberto.carazo@gmail.com>
2023-10-25 18:17:55 +0200

    Localstorage + transitions

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const API = "https://api.github.com/users/";
 
+const maxTime = 5000;
+
 const app = Vue.createApp({
     data() {
         return {
@@ -10,9 +12,16 @@ const app = Vue.createApp({
             favorites: new Map()
         }
     },
+    created() {
+        const savedFavorites = JSON.parse(localStorage.getItem('favorite'))
+        if (savedFavorites?.length) { // -> https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Operators/Optional_chaining
+            const favorites = new Map(savedFavorites.map( item => [item.login, item]))
+            this.favorites = favorites
+        }
+    },
     computed:{
         isFavorite() {
-            return this.favorites.has(this.result.id)
+            return this.favorites.has(this.result.login)
         },
         allFavorites() {
             return Array.from(this.favorites.values())
@@ -21,11 +30,30 @@ const app = Vue.createApp({
     methods: {
         async doSearch() {
             this.result = this.error = null;
+
+            const existSavedFavorite = this.favorites.get(this.search)
+            const requestFavorite = (() => {
+                if (!!existSavedFavorite) {
+                    const { time } = existSavedFavorite
+                    const now = Date.now()
+                    // console.log(`han pasado 5 segunddos? ${now} - ${time} = ${(now - time)} -> ${(now - time) > maxTime}`);
+                    return (now - time) > maxTime
+                }
+                return false
+            })()
+
+
+            // Sobre el uso de !!   -> https://chat.openai.com/share/1184a730-8722-404a-86d4-042fe960b8c8
+            //                      -> https://g.co/bard/share/20c276fcbb9f
+            if (!!existSavedFavorite && !requestFavorite) {
+                console.log("Found and we use the cached version")
+                return this.result = existSavedFavorite;
+            }
+            
             try {
                 const resp = await fetch(API + this.search);
                 if(!resp.ok) throw new Error("Sin resultado")
                 const data = await resp.json();
-                console.log(data);
                 
                 this.result = data;
             } catch (error) {
@@ -35,10 +63,18 @@ const app = Vue.createApp({
             }
         },
         addFavorite() {
-            this.favorites.set(this.result.id, this.result)
+            this.result.time = Date.now();
+            this.favorites.set(this.result.login, this.result);
+            this.setLocalStorage();
+        },
+        showFavorite(favorite) {
+            this.result = favorite
         },
         removeFavorite() {
-            this.favorites.delete(this.result.id)
+            this.favorites.delete(this.result.login)
+        },
+        setLocalStorage() {
+            localStorage.setItem('favorite',JSON.stringify(this.allFavorites))
         }
     }
 }).mount('#app')
